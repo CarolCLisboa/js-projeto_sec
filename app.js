76cb4c943c4896297c47cd0f712ba81bf054a430
Carolina Calli <carolina.calli46@gmail.com>
2025-03-21 21:50:45 -0300

    Upgrade

--- app.js
+++ app.js
@@ -1,37 +1,42 @@
-<<<<<<< HEAD
-let titulo = document.querySelector ('h1');
-titulo.innerHTML = 'Jogo da Adivinhação';
-
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo de Adivinhação';
+ 
 let subtitulo = document.querySelector('p');
-subtitulo.innerHTML = 'Escolha um número de 1 e 100';
-
-exibirTextoNaTela('h1' ,'Jogo de Adivinhação');
-exibirTextoNaTela('p' , ' Escolha um número entre 1 e 100');
-
-function exibirTextoNaTela(tag, texto) {
-    let campo = document.querySelector (tag);
-=======
-// let titulo = document.querySelector('h1');
-// titulo.innerHTML = 'Jogo de adivinhação';
-
-// let subtitulo = document.querySelector('p');
-// subtitulo.innerHTML = 'Escolha um número entre 1 e 100';
-
+subtitulo.innerHTML = ' Escolha um número de 1 e 100';
+ 
+let tentativas = 3;
+ 
 exibirTextoNaTela('h1', 'Jogo de adivinhação');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
-
+ 
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 10) + 1;
+}
+ 
+let numeroSecreto = gerarNumeroAleatorio();
+console.log (numeroSecreto);  
+ 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
->>>>>>> ed78fcbe0f71bbd87459d9d06682a62e04fd8bfb
     campo.innerHTML = texto;
 }
-
+ 
 function verificarChute() {
-    console.log('Apertou o botão');
+    let chute = parseInt (document.querySelector('input').value);
+    if(tentativas === 0){
+        exibirTextoNaTela('h1', 'Suas tentativas acabaram!');
+    } else {
+        tentativas--;
+        let palavraTentativa = tentativas === 1? 'tentativa' : 'tentativas';
+    if(chute  === numeroSecreto) {
+      exibirTextoNaTela ('h1', 'Parabéns! Você acertou!');
+    } else if (chute > numeroSecreto){
+        exibirTextoNaTela('h1', `Errou! o numero secreto é menor.
+            Você ainda tem ${tentativas} ${palavraTentativa}`);
+    } else {
+        exibirTextoNaTela('h1', `Errou! o numero secreto é maior.
+        Você ainda tem ${tentativas} ${palavraTentativa}.`);
+    }
 }
-<<<<<<< HEAD
-=======
-
-//<h1>Jogo de adivinhação</h1>
-//<p>Escolha um número entre 1 e 100</p>
->>>>>>> ed78fcbe0f71bbd87459d9d06682a62e04fd8bfb
+}
+ 
